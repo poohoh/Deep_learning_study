44987769c92c928f93342b3d011d24070bcb1ec8
poohoh <kjh980328@gmail.com>
2024-01-26 16:50:30 +0900

    Day4 - mnist classification using mlp

---
+++ Day4/mnist_classification.py
@@ -0,0 +1,69 @@
+import torch
+import torchvision
+import torch.nn.functional as F
+from torchvision import transforms
+from torch.utils.data.dataloader import DataLoader
+
+# device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# Reproducibility
+torch.manual_seed(123)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(123)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+# setup image set
+train_X = torchvision.datasets.MNIST('./data', train=True, download=True, transform=transform)
+test_X = torchvision.datasets.MNIST('./data', train=False, download=True, transform=transform)
+
+# setup data loader
+train_loader = DataLoader(train_X, batch_size=64, shuffle=True, drop_last=True, pin_memory=True)
+test_loader = DataLoader(test_X, batch_size=128, shuffle=False, drop_last=False, pin_memory=True)
+
+# Model
+layer = torch.nn.Sequential(
+    torch.nn.Flatten(),
+    torch.nn.Linear(in_features=784, out_features=256, bias=True),
+    torch.nn.ReLU(),
+    torch.nn.Linear(in_features=256, out_features=10, bias=True)
+).to(device)
+
+print(layer)
+
+# Optimizer
+optimizer = torch.optim.Adam(layer.parameters(), lr=0.001)
+
+# Training
+for epoch in range(15):
+    for idx, (images, labels) in enumerate(train_loader):
+        # data to cuda
+        images, labels = torch.FloatTensor(images).to(device), torch.LongTensor(labels).to(device)
+
+        # output
+        output = layer(images)
+
+        # loss
+        loss = F.cross_entropy(input=output, target=labels)
+
+        # gradient initialize
+        optimizer.zero_grad()
+
+        # calculate gradient
+        loss.backward()
+
+        # update parameters
+        optimizer.step()
+
+        # calculate accuracy
+        prob = F.softmax(output, dim=1)
+        pred = torch.argmax(prob, dim=1)
+        accuracy = torch.eq(pred, labels).float().mean()
+        if idx % 128 == 0:
+            print(f'train iteration: {idx+1}, loss: {loss.item()}')
+
+    print(f'epoch: {epoch+1} completed\n')
