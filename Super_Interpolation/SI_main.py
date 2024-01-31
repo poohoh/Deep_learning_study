fa64dc7eb89eee4fd1e1493f43793529f036d488
poohoh <kjh980328@gmail.com>
2024-01-31 22:29:20 +0900

    fix super interpolation - concatenate clusters, not appending

--- Super_Interpolation/SI_main.py
+++ Super_Interpolation/SI_main.py
@@ -1,10 +1,19 @@
 from training import *
+import os
 
-img = cv2.imread('./Data/Training/t1.bmp')
-img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
-y, cb, cr = cv2.split(img)
+# img = cv2.imread('./Data/Training/t1.bmp')
+# img = cv2.cvtColor(img, cv2.COLOR_BGR2YCrCb)
+
+data_dir = './Data/Training'
+images = os.listdir(data_dir)
+hr_images = []
+for image in images[:10]:
+    img = cv2.imread(os.path.join(data_dir, image))
+    hr_images.append(img)
+
+lin_map = training(hr_images)
 
 # merged = cv2.merge([y, cb, cr])
 # merged = cv2.cvtColor(merged, cv2.COLOR_YCrCb2BGR)
 
-training(y)
+print(lin_map)
